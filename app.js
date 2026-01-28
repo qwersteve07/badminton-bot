a3ff5b9440b8ca3bebbdfda5985d3719cad2fa6b
sslee02 <Steve-SH.Lee@nanshan.com.tw>
2026-01-28 17:51:46 +0800

    feat: update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { chromium } from "@playwright/test";
 import { getNextTargetDate } from "./utils/get-date.js";
-import { sendLineNotify } from "./utils/line-notify.js";
 import "dotenv/config";
 
 const pickTimes = ["20", "21"];
@@ -17,7 +16,7 @@ export const run = async () => {
 	const page = await context.newPage();
 	// Navigate to login
 	await page.goto(
-		`${targetSportsCenterUrl}${targetSportsCenterHomePath}?module=login_page&files=login&PT=1`
+		`${targetSportsCenterUrl}${targetSportsCenterHomePath}?module=login_page&files=login&PT=1`,
 	);
 
 	// login
@@ -29,13 +28,13 @@ export const run = async () => {
 	await page.waitForSelector("img[title=我的訂單]");
 
 	// wait for midnight
-	let isAvailable = new Date().toTimeString().includes("00:00:00");
+	let isAvailable = new Date().toTimeString().includes("23:59:59");
 	while (!isAvailable) {
-		isAvailable = new Date().toTimeString().includes("00:00:00");
+		isAvailable = new Date().toTimeString().includes("23:59:59");
 	}
 
 	// pick place
-	const pickPlaceResponse = await page.evaluate(
+	await page.evaluate(
 		async ({
 			pickTimes,
 			date,
@@ -45,9 +44,9 @@ export const run = async () => {
 			return await Promise.all(
 				pickTimes.map(async (time) => {
 					return fetch(
-						`${targetSportsCenterUrl}${targetSportsCenterHomePath}?module=net_booking&files=booking_place&StepFlag=25&QPid=84&PT=1&D=${date}&QTime=${time}`
+						`${targetSportsCenterUrl}${targetSportsCenterHomePath}?module=net_booking&files=booking_place&StepFlag=25&QPid=84&PT=1&D=${date}&QTime=${time}`,
 					).then((res) => res.text());
-				})
+				}),
 			);
 		},
 		{
@@ -55,40 +54,10 @@ export const run = async () => {
 			date: nextTargetDate,
 			targetSportsCenterUrl,
 			targetSportsCenterHomePath,
-		}
-	);
-
-	// get pick place result path
-	const pickPlaceResultPath = pickPlaceResponse.map((res) => {
-		const path = res.substring(
-			res.indexOf("../../..") + 8,
-			res.indexOf("' </script>")
-		);
-		return `${targetSportsCenterUrl}${path}`;
-	});
-
-	// get pick place result
-	const pickPlaceResult = await page.evaluate(
-		async ({ pickPlaceResultPath }) => {
-			return await Promise.all(
-				pickPlaceResultPath.map((path) =>
-					fetch(path).then((res) => res.text())
-				)
-			);
 		},
-		{ pickPlaceResultPath }
 	);
-
-	// send notify
-	pickPlaceResult.forEach(async (res, index) => {
-		if (res.indexOf("預約成功") !== -1) {
-			sendLineNotify(
-				`預約成功🏸🏸🏸 \t\n日期: ${nextTargetDate}\t\n時段: ${pickTimes[index]}`
-			);
-		} else {
-			sendLineNotify(`預約失敗`);
-		}
-	});
 
 	await browser.close();
 };
+
+run();
