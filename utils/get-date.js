115b210f782b8cbeb612e434a4204a778e0427c4
sslee02 <Steve-SH.Lee@nanshan.com.tw>
2026-02-03 13:54:47 +0800

    feat: use dayjs()

--- utils/get-date.js
+++ utils/get-date.js
@@ -1,13 +1,13 @@
+import dayjs from "dayjs";
+import utc from '"dayjs/plugin/utc"';
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 // 取得下週時間
 export const getNextTargetDate = () => {
-  // 取得目前日期
-  let today = new Date();
-  let targetDate = new Date(today);
-  targetDate.setDate(targetDate.getDate() + 7);
-
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let day = targetDate.getDate();
-
-  return `${year}/${month}/${day}`;
+	// 取得目前日期
+	let today = dayjs();
+	let targetDate = dayjs().add(7, "d");
+	return targetDate.format("YYYY/MM/DD");
 };
