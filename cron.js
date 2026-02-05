8e282ca5576913e34d56978331f6cce91ce73887
sslee02 <Steve-SH.Lee@nanshan.com.tw>
2026-02-05 10:41:28 +0800

    feat: add timezone

--- cron.js
+++ cron.js
@@ -2,11 +2,11 @@ import { CronJob } from "cron";
 import { run } from "./app.js";
 
 new CronJob(
-  "0 59 23 * * *", // cronTime
-  function () {
-    run();
-  }, // onTick
-  null, // onComplete
-  true, // start
+	"0 59 23 * * *",
+	function () {
+		run();
+	},
+	null,
+	true,
+	"Asia/Taipei",
 );
-//
